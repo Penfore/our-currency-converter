69ec31d902aaf834385439235ba6f2fd0919e67f
Fúlvio Leo <fulviokaldas@outlook.com>
2025-09-10 10:17:49 -0300

    Initial Commit

---
+++ Our Currency Converter/Our_Currency_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Our_Currency_ConverterApp.swift
+//  Our Currency Converter
+//
+//  Created by Fulvio Leo on 10/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Our_Currency_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Our Currency ConverterTests/Our_Currency_ConverterTests.swift
@@ -0,0 +1,17 @@
+//
+//  Our_Currency_ConverterTests.swift
+//  Our Currency ConverterTests
+//
+//  Created by Fulvio Leo on 10/09/25.
+//
+
+import Testing
+@testable import Our_Currency_Converter
+
+struct Our_Currency_ConverterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
